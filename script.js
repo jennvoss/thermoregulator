1007c307d656ac0336377c69c7cf4746f4f2f7a3
Jenn Voss <jenn.voss@gmail.com>
2016-12-11 13:37:21 -0500

    Remove jquery and bootstrap

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-var toggleOutlet = function(buttonClicked) {
-    $.post('toggle.php', {
-            outletId: buttonClicked.attr('data-outletId'),
-            outletStatus: buttonClicked.attr('data-outletStatus')
-        },
-        function(data, status) {
-            //alert('Outlet toggled!');
-        });
+(function() {
 
-};
+  var buttons = document.querySelectorAll('.toggleOutlet');
+  
+  Array.from(buttons).forEach(link => {
+    link.addEventListener('click', toggleOutlet.bind(link, link.getAttribute('data-outletId'), link.getAttribute('data-outletStatus')));
+  });
 
-$(function() {
-    $('.toggleOutlet').click(function() {
-        toggleOutlet($(this));
-    });
-});
+  function toggleOutlet(outletId, outletStatus) {
+    var data = 'outletId=' + outletId + '&outletStatus=' + outletStatus;
+
+    var http = new XMLHttpRequest();
+    http.open('POST', '/toggle', true);
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    http.onreadystatechange = function() {
+      if (http.readyState == 4 && http.status == 200) {
+        console.log(http.responseText);
+      }
+    }
+    http.send(data);
+  }
+
+}());
