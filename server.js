68d26aba080b8a11dc4a9690570089121cc5c983
Jenn Voss <vossj@ELSPHIM-4170418.science.regn.net>
2016-12-15 15:39:44 -0500

    Write temperature values to firebase

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const toggle = require('./toggle.js');
 const bean = require('./bean.js');
 const tempEmitter = bean.temperature();
 
-const admin = require('firebase-admin');
+const fb_admin = require('firebase-admin');
+const fb_db = require('firebase/database'); // only needed for ServerValue.TIMESTAMP
 const fb_serviceAccount = require('./secret/thermoregulator-67d13-firebase-adminsdk-kwc9p-897e8a0389.json');
 const fb_db_url = "https://thermoregulator-67d13.firebaseio.com";
 
@@ -58,29 +59,36 @@ server.register(require('inert'), (err) => {
 
 });
 
-// temp reader
-tempEmitter.startPolling({ interval: 30 });
-tempEmitter.on('temperature', (temp)=> {
-  console.log('here is the temp: ', temp);
-});
-
-tempEmitter.on('error', (err)=> {
-  console.log('error: ', err);
-  tempEmitter.quitGracefully();
-});
 
 // firebase
-admin.initializeApp({
-  credential: admin.credential.cert(fb_serviceAccount),
+fb_admin.initializeApp({
+  credential: fb_admin.credential.cert(fb_serviceAccount),
   databaseURL: fb_db_url
 });
 
-const db = admin.database();
+const db = fb_admin.database();
 const threshold = db.ref('/threshold');
+const tempList = db.ref('/temps');
 
 threshold.on('value', function(snapshot) {
   lowTemp = snapshot.low;
   highTemp = snapshot.high;
 }, function (errorObject) {
   console.log('The read failed: ' + errorObject.code);
+});
+
+
+// temp reader
+tempEmitter.startPolling({ interval: 30 });
+tempEmitter.on('temperature', (temp)=> {
+  let newTempRef = tempList.push();
+  newTempRef.set({
+    temperature: temp,
+    timestamp: fb_db.ServerValue.TIMESTAMP
+  });
+});
+
+tempEmitter.on('error', (err)=> {
+  console.log('error: ', err);
+  tempEmitter.quitGracefully();
 });
