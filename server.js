0c204fb3ce447a2062d66febad63dec52a336076
Jenn Voss <vossj@ELSPHIM-4170418.science.regn.net>
2016-12-15 15:10:54 -0500

    Setup firebase, read temp thresholds

--- server.js
+++ server.js
@@ -2,12 +2,21 @@
 
 const Hapi = require('hapi');
 const server = new Hapi.Server();
+
 const toggle = require('./toggle.js');
 const bean = require('./bean.js');
 const tempEmitter = bean.temperature();
 
-server.connection({ port: 3000 });
+const admin = require('firebase-admin');
+const fb_serviceAccount = require('./secret/thermoregulator-67d13-firebase-adminsdk-kwc9p-897e8a0389.json');
+const fb_db_url = "https://thermoregulator-67d13.firebaseio.com";
 
+// high and low temp defaults (celcius)
+let lowTemp = 29; // 84.2 F
+let highTemp = 32; // 89.6 F
+
+// web server
+server.connection({ port: 3000 });
 server.register(require('inert'), (err) => {
   if (err) {
     throw err;
@@ -49,10 +58,8 @@ server.register(require('inert'), (err) => {
 
 });
 
-tempEmitter.startPolling({
-  interval: 30
-});
-
+// temp reader
+tempEmitter.startPolling({ interval: 30 });
 tempEmitter.on('temperature', (temp)=> {
   console.log('here is the temp: ', temp);
 });
@@ -60,4 +67,20 @@ tempEmitter.on('temperature', (temp)=> {
 tempEmitter.on('error', (err)=> {
   console.log('error: ', err);
   tempEmitter.quitGracefully();
+});
+
+// firebase
+admin.initializeApp({
+  credential: admin.credential.cert(fb_serviceAccount),
+  databaseURL: fb_db_url
+});
+
+const db = admin.database();
+const threshold = db.ref('/threshold');
+
+threshold.on('value', function(snapshot) {
+  lowTemp = snapshot.low;
+  highTemp = snapshot.high;
+}, function (errorObject) {
+  console.log('The read failed: ' + errorObject.code);
 });
