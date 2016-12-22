9e7131e92ff7be9197d01408f39d56949b9928bd
Jenn Voss <vossj@ELSPHIM-4170418.science.regn.net>
2016-12-22 10:44:06 -0500

    Update toggle script

--- toggle.js
+++ toggle.js
@@ -1,7 +1,3 @@
-module.exports = function (data) {
-  toggleSwitch(data);
-}
-
 function run_cmd(cmd, callBack) {
   const util = require('util');
   const exec = require('child_process').exec;
@@ -18,32 +14,28 @@ function toggleSwitch(data) {
   const codeSendPIN = "0";
 
   // Pulse length depends on the RF outlets you are using. Use RFSniffer to see what pulse length your device uses.
-  const codeSendPulseLength = "189";
+  const codeSendPulseLength = "184";
 
   const codes = {
     "1": {
-      "on": 349491,
-      "off": 349500
+      "on": 1070387,
+      "off": 1070396
     },
     "2": {
-      "on": 349635,
-      "off": 349644
+      "on": 1070531,
+      "off": 1070396
     },
     "3": {
-      "on": 349955,
-      "off": 349964
-    },
-    "4": {
-      "on": 351491,
-      "off": 351500
-    },
-    "5": {
-      "on": 357635,
-      "off": 357644
-    },
+      "on": 1070851,
+      "off": 1070860
+    }
   };
 
   const codeToToggle = codes[data.outletId][data.outletStatus];
 
   run_cmd('sudo ' + codeSendPath + ' ' + codeToToggle + ' -p ' + codeSendPIN + ' -l ' + codeSendPulseLength);
+}
+
+module.exports = function(data) {
+  toggleSwitch(data);
 }
